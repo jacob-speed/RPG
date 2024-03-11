a4dcc017e8e456a3699c1c757c19f2f084c76905
Jacob Kael <jkmans11@hotmail.com>
2024-03-11 17:40:45 +1100

    Updated #todo list

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
 # todo expand generate_map() function to create randomised maps
 # todo start making attacks to damage, kill enemies lose hit points
 # todo improvements to collisions
-# create more player attributes
-# create map tiles
-# create items
-# create skills/ attacks
-# create functions to interact with files for items, monsters, skills, map tiles etc.
+# todo create more player attributes
+# todo create map tiles
+# todo create items
+# todo create skills/ attacks
+# todo create functions to interact with files for items, monsters, skills, map tiles etc.
